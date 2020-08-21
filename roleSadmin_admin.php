e79a3908b62455edd602b9ce9ee4fa03d77c8bec
Jerezchristophe <70036579+Jerezchristophe@users.noreply.github.com>
2020-08-21 22:44:11 +0200

    Delete roleSadmin_admin.php

--- roleSadmin_admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if($_SESSION['PROFILE']['idRole'] != 2 AND $_SESSION['PROFILE']['idRole'] != 0){// si le role de la session en cour est différent du role super admin et admin
-        session_destroy();
-        header("location:../index.php");// on reste sur la meme page
-    }
-?>
